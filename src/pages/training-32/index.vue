63d39ae1c92193cdeb189daf94f469216dcd686f
Michal Ryngier <michal.ryngier@rebilly.com>
2025-03-25 16:00:06 +0100

    feat(phronesis-32): created Phronesis 32

--- src/pages/training-32/index.vue
+++ src/pages/training-32/index.vue
@@ -27,28 +27,36 @@
 
     <v-divider class="mb-4 mt-4" />
 
-    <h2 class="font-weight-medium">
-      Confusing things (7)
-    </h2>
-
     <v-list>
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           Adding permissions is not user friendly. Having to copy-paste them from the documentation is hard, there is no way to search for them (browser's search does not see the textarea content), textarea cannot be expanded.
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           No information how permissions have to be written (comma/space/new line separated).
         </v-list-item-content>
@@ -62,11 +70,26 @@
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title class="d-flex">
+          <div class="rounded-pill bg-red w-10 mr-2">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-bug
+            </v-icon>
+            <span class="font-weight-bold">Issue</span>
+          </div>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           Empty actions list when insufficient permissions.
         </v-list-item-content>
@@ -80,11 +103,26 @@
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title class="d-flex">
+          <div class="rounded-pill bg-red w-10 mr-2">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-bug
+            </v-icon>
+            <span class="font-weight-bold">Issue</span>
+          </div>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           Various UI elements visible when the user has no permission to access them. (example: KPIs, Automations)
         </v-list-item-content>
@@ -98,11 +136,17 @@
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           It is not clear which permissions will be used when creating/updating the user.
           We think it should be possible to use role's permissions without any modification.
@@ -117,11 +161,26 @@
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title class="d-flex">
+          <div class="rounded-pill bg-red w-10 mr-2">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-bug
+            </v-icon>
+            <span class="font-weight-bold">Issue</span>
+          </div>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           Sometimes when creating the user error is displayed but the user is created. We don't know why it sometimes works and sometimes doesn't.
         </v-list-item-content>
@@ -140,11 +199,26 @@
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-icon>
-          <v-icon color="orange">
-            mdi-alert-circle
-          </v-icon>
-        </v-list-item-icon>
+        <v-list-item-title class="d-flex">
+          <div class="rounded-pill bg-red w-10 mr-2">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-bug
+            </v-icon>
+            <span class="font-weight-bold">Issue</span>
+          </div>
+          <div class="rounded-pill bg-orange w-10">
+            <v-icon
+              color="white"
+              class="mr-2"
+            >
+              mdi-alert-circle
+            </v-icon>
+            <span class="font-weight-bold">Confusing</span>
+          </div>
+        </v-list-item-title>
         <v-list-item-content>
           We couldn't access creating billing portal link despite having all permissions related to the billing portal link. We needed to check the network tab to see that we need <code>PostAuthenticationToken</code> permission.
         </v-list-item-content>
@@ -161,5 +235,7 @@
 </template>
 
 <style scoped lang="scss">
-
+.w-10 {
+  width: 10%;
+}
 </style>
