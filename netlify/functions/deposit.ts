df7e904de6c7d90b45adc55d7de7f9a3a71ff59b
Michal Ryngier <michal.ryngier@rebilly.com>
2025-08-29 10:37:41 +0200

    feat(p54): Phronesis 54

--- netlify/functions/deposit.ts
+++ netlify/functions/deposit.ts
@@ -1,18 +1,15 @@
-import type { Context } from "@netlify/functions";
-
 import RebillyAPI from "rebilly-js-sdk";
+import type {Context} from "@netlify/functions";
 
-const REBILLY_API_SECRET_KEY = "sk_sandbox_lNFYj-AUBEko3QZ-s4XSaUMwPUMQiXfJ4fs8vE3";
-const REBILLY_WEBSITE_ID = "www.luckyclovercasino.com";
-const REBILLY_ORGANIZATION_ID = "phronesis-lucky-clover-casino";
-const CUSTOMER_ID = "cus_01JPMQYSZZV8X6SQCWS27D3KCX";
-const DEPOSIT_STRATEGY_ID = "dep_str_01JQEMWX4SJHBAMRMSP5REWWDF";
+const API_SECRET_KEY = "sk_sandbox_ZFRju65t2NoS6AjSG5oBsXALLBNmwXj_JO8i6bO";
+const ORGANIZATION_ID = "phronesis-casino-la-blanca";
+const WEBSITE_ID = "lablanca-casino.com";
 
 const rebilly = RebillyAPI({
+  organizationId: ORGANIZATION_ID,
   sandbox: true,
-  organizationId: REBILLY_ORGANIZATION_ID,
-  apiKey: REBILLY_API_SECRET_KEY,
-  timeout: 10000,
+  apiKey: API_SECRET_KEY,
+  timeout: 5000,
 });
 
 export default async (req: Request, context: Context) => {
@@ -27,79 +24,32 @@ export default async (req: Request, context: Context) => {
 
   const response: {
     token?: string,
-    depositRequestId?: string
   } = {};
 
-  const data = {
-    mode: "passwordless",
-    customerId: CUSTOMER_ID,
-  };
-
-  const { fields: login } = await rebilly.customerAuthentication.login({
-    data,
-  });
-
-  const { fields: exchangeToken } =
-    await rebilly.customerAuthentication.exchangeToken({
-      token: login.token,
+  try {
+    const cachierResponse = await rebilly.cashiers.create({
       data: {
-        acl: [
-          {
-            scope: {
-              organizationId: [REBILLY_ORGANIZATION_ID],
-            },
-            permissions: [
-              "PostToken",
-              "PostDigitalWalletValidation",
-              "StorefrontGetAccount",
-              "StorefrontPatchAccount",
-              "StorefrontPostPayment",
-              "StorefrontGetTransactionCollection",
-              "StorefrontGetTransaction",
-              "StorefrontGetPaymentInstrumentCollection",
-              "StorefrontPostPaymentInstrument",
-              "StorefrontGetPaymentInstrument",
-              "StorefrontPatchPaymentInstrument",
-              "StorefrontPostPaymentInstrumentDeactivation",
-              "StorefrontGetWebsite",
-              "StorefrontGetInvoiceCollection",
-              "StorefrontGetInvoice",
-              "StorefrontGetProductCollection",
-              "StorefrontGetProduct",
-              "StorefrontPostReadyToPay",
-              "StorefrontGetPaymentInstrumentSetup",
-              "StorefrontPostPaymentInstrumentSetup",
-              "StorefrontGetDepositRequest",
-              "StorefrontGetDepositStrategy",
-              "StorefrontPostDeposit",
-            ],
-          },
-        ],
-        customClaims: {
-          websiteId: REBILLY_WEBSITE_ID,
-        },
+        websiteId: WEBSITE_ID,
+        customerId: "cus_01K3TEGEH60DD2DTXDFKSMG6HN",
+        currency: "USD",
       },
     });
 
-  const requestDepositData = {
-    websiteId: REBILLY_WEBSITE_ID,
-    customerId: CUSTOMER_ID,
-    strategyId: DEPOSIT_STRATEGY_ID,
-    currency: "USD",
-  };
+    response.token = cachierResponse.fields.cashierToken
 
-  const { fields: depositFields } = await rebilly.depositRequests.create({
-    data: requestDepositData,
-  });
+    const responseObj = new Response(JSON.stringify(response));
 
-  response.token = exchangeToken.token;
-  response.depositRequestId = depositFields.id;
+    responseObj.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    responseObj.headers.append('Access-Control-Allow-Methods', 'POST');
+    responseObj.headers.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    responseObj.headers.append('Access-Control-Allow-Credentials', 'true');
 
-  const responseObj = new Response(JSON.stringify(response));
-
-  responseObj.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-  responseObj.headers.append('Access-Control-Allow-Methods', 'POST');
-  responseObj.headers.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  responseObj.headers.append('Access-Control-Allow-Credentials', 'true');
-  return responseObj;
+    return responseObj;
+  } catch (error) {
+    if (error?.response?.data) {
+      console.error(error.response.data);
+    } else {
+      console.error(error);
+    }
+  }
 }
--- src/pages/casino/deposit/index.vue
+++ src/pages/casino/deposit/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import RebillyInstruments, {type ThemeProperties} from '@rebilly/instruments';
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -12,42 +11,21 @@ const apiUrl = import.meta.env.VITE_API_BASE_URL;
     },
   });
 
-  const {token, depositRequestId} = await response.json();
+  const data = await response.json();
 
-  console.log({token, depositRequestId});
+  const mountElement = document.querySelector("#deposit");
+  const cashierToken = data.token;
 
-  const theme: ThemeProperties = {
-    colorPrimary: '#008F39',
-    colorText: '#FFD700',
-    colorBackground: '#002B16',
-    colorDanger: '#CF6679',
-    buttonColorBackground: '#008F39',
-    buttonColorText: '#FFD700',
-    buttonSecondaryBorder: '1px solid #4A2E00',
-    inputBorder: '1px solid #4A2E00',
-    colorSuccessDark: '#002B16',
-    colorSuccessMuted: '#008F39'
+  console.log({cashierToken});
+
+  if (window.RebillyCashier) {
+    window.RebillyCashier.renderDeposit({
+      mountElement,
+      cashierToken,
+    });
+  } else {
+    console.error("RebillyCashier library not loaded");
   }
-
-  // Mount Rebilly Instruments
-  RebillyInstruments.mount({
-    apiMode: "sandbox",
-    deposit: {
-      depositRequestId,
-    },
-    jwt: token,
-    css: '.rebilly-instruments-amount-selector { background: #002B16; }' +
-      '.rebilly-instruments-form-field-label { color: #FFD700 }' +
-      'a:link { color: #008F39 } a:visited { color: #4A2E00 }',
-    theme: theme
-  });
-  // Optional
-  RebillyInstruments.on("instrument-ready", (instrument) => {
-    console.info("instrument-ready", instrument);
-  });
-  RebillyInstruments.on("purchase-completed", (purchase) => {
-    console.info("purchase-completed", purchase);
-  });
 })();
 </script>
 
@@ -55,8 +33,7 @@ const apiUrl = import.meta.env.VITE_API_BASE_URL;
   <v-row>
     <v-col>
       <div class="deposit-form d-flex flex-column align-center">
-        <div class="rebilly-instruments-summary" />
-        <div class="rebilly-instruments" />
+        <div id="deposit" />
       </div>
     </v-col>
   </v-row>
@@ -77,27 +54,7 @@ const apiUrl = import.meta.env.VITE_API_BASE_URL;
 </style>
 
 <style lang="scss">
-//#rebilly-instruments-app {
-//  background: transparent !important;
-//  padding: 8px !important;
-//}
-
-.rebilly-instruments {
-  .rebilly-instruments-content {
-    padding: 8px;
-    border-radius: 4px;
-    box-shadow: 0 4px 8px 0 #008F39, 0 6px 20px 0 #008F39;
-
-    .form {
-      border-radius: 2px;
-      padding: 4px;
-    }
-  }
-}
-
-.rebilly-instruments-summary {
-  .rebilly-instruments-iframe {
-    border-radius: 4px;
-  }
+.r-button {
+  background: #f5f5f5;
 }
 </style>
